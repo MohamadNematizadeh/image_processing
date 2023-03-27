b180434da04d933da42dab15a06ba165c027c304
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2023-03-27 13:35:37 +0330

    Update 1.py

--- Assignment 27/1.py
+++ Assignment 27/1.py
@@ -3,6 +3,7 @@ import cv2
 img = cv2.imread('input/bat.jpg' , 0)
 height , width = img.shape
 _, img=cv2.threshold(img,100,255,cv2.THRESH_BINARY_INV)
+cv2.putText(img, 'BATMAN', (300, 550), cv2.FONT_HERSHEY_SIMPLEX, 3, 255, thickness= 5)
 cv2.imshow('', img)
 cv2.imwrite('output/snowy_wolf.jpg' , img)
 cv2.waitKey()
