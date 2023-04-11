a442753260e86bf089f98cf243d4b6cde0e18214
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2023-04-11 22:51:18 +0330

    Update Face Morphing.py

--- Assignment29/Face Morphing.py
+++ Assignment29/Face Morphing.py
@@ -1,16 +1,25 @@
+
 import cv2
 import numpy as np
 
+image_Obama= cv2.imread("input/Face Morphing/Obama.jpg")
+image_Joe_Biden =cv2.imread("input/Face Morphing/Joe_Biden.jpg")
+image_Joe_Biden = image_Joe_Biden[10:,:,:]
+w, h, _ = image_Joe_Biden.shape
+image_Obama = cv2.resize(image_Obama, [h, w])
 
-img_1 = cv2.imread("input/Face Morphing/Trump.jpg")
-img_2= cv2.imread("input/Face Morphing/Trump2.jpg")
+image_Obama = image_Obama.astype(np.float32)
+image_Joe_Biden = image_Joe_Biden.astype(np.float32)
 
-# img_1 = cv2.cvtColor(img_1,cv2.COLOR_BGR2GRAY)
-# img_2 = cv2.cvtColor(img_2,cv2.COLOR_BGR2GRAY)
+image_Joe_Biden_robert1 = image_Obama*.25 + image_Joe_Biden*.75
+image_Joe_Biden_robert2 = image_Obama*.5 + image_Joe_Biden*.5
+image_Joe_Biden_robert3 = image_Obama*.75 + image_Joe_Biden*.25
 
-# img_1 = img_1.astype(np.float32)
-# img_2= img_2.astype(np.float32)
-# result=img_1img_2
-# result = result.astype(np.uint8)
+image_Joe_Biden_robert1 = image_Joe_Biden_robert1.astype(np.uint8)
+image_Joe_Biden_robert2 = image_Joe_Biden_robert2.astype(np.uint8)
+image_Joe_Biden_robert3 = image_Joe_Biden_robert3.astype(np.uint8)
 
-cv2.imwrite("output/result2.jpg",img_1)
+image_Joe_Biden_robert = np.concatenate((image_Joe_Biden, image_Joe_Biden_robert1, image_Joe_Biden_robert2,
+ image_Joe_Biden_robert3, image_Obama), axis=1)
+
+cv2.imwrite('output/image_Obama_and_Joe_Biden.jpg',image_Joe_Biden_robert)
