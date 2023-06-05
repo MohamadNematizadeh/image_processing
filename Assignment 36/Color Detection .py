045a37760e6c400bb497d840842674a3f1d85763
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2023-06-05 19:26:03 +0330

    Add files via upload

---
+++ Assignment 36/Color Detection .py
@@ -0,0 +1,48 @@
+import cv2
+cap = cv2.VideoCapture(0)
+_, frame = cap.read()
+rows = frame.shape[0]
+cols = frame.shape[1]
+while True:
+    _, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    height, width, _ = frame.shape
+
+    cx = int(width / 2)
+    cy = int(height / 2)
+
+    pixel_center = frame[cy, cx]
+    hue_value = pixel_center[0]
+
+    color = "Undefined"
+    if hue_value < 5:
+        color = "RED"
+    elif hue_value < 22:
+        color = "ORANGE"
+    elif hue_value < 33:
+        color = "YELLOW"
+    elif hue_value < 78:
+        color = "GREEN"
+    elif hue_value < 85:
+        color = "BLAK"
+    elif hue_value < 131:
+        color = "BLUE"
+    elif hue_value < 170:
+        color = "VIOLET"
+    elif hue_value < 255:
+        color = "White"    
+    else:
+        color = "Grey"
+
+    pixel_center_bgr = frame[cy, cx]
+    H, S, V = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+    cv2.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
+    cv2.putText(frame, color, (cx - 200, 100), 0, 3, (H, S, V), 5)
+
+    cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
+    frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break   
+cap.release()
+cv2.destroyAllWindows()
